7a08cd8e3ada817bee9fa38a68bdc75f39d2c48d
decoNR <94117783+decoNR@users.noreply.github.com>
2022-05-29 18:45:20 -0300

    prototipo inicial decompress

--- decompress.m
+++ decompress.m
@@ -1,13 +1,53 @@
 function [decompressedImg] = decompress (compressedImg, method, k, h)
   % função que, recebendo uma imagem comprimida, faz a sua descompressão
   compressed = imread (compressedImg); % compressed é uma matriz NxNx3
+  
   n = rows(compressed);
   p = n + (n - 1) * k;
-  img = nan(p, p, 3); % cria matriz PxPx3 com valores NaN
-  for i = 1:n
-    for j = 1:n
-      printf("z(%d,%d) = %f\n", i, j, compressed(i,j));
+  img = zeros(p, p, 3); % cria matriz PxPx3 com valores NaN
+  
+  d = h/(k+1) %d é a distancia entre cada ponto e seu adjacente
+  
+  fr = compressed(:,:,1);
+  fg = compressed(:,:,2);
+  fb = compressed(:,:,3);
+  fs = [fr, fg, fb];
+  
+  coefsl = nan(4);
+  coefsb = nan(4,4);
+  
+  %retirar dps
+  k = 1;
+  
+  x0 = 0;
+  y0 = 0;
+  
+  for i = 1:n-1
+    for j = 1:n-1
+      %achar o polinomio para um quadrado    
+      pontos = [i,j;i,j+1; i+1,j; i+1,j+1];
+      coefsb = bicubico(fs[k], pontos);     
+
+      %colocar na matriz pxp 
+      lini = (((i-1)*k)+i);
+      cini = (((j-1)*k)+j);
+      % xi = x0 + i*h
+      xi = x0 + i*h;
+      yj = y0 + j*h;
+      for a = lini:(i*k)+(i+1)
+        for b = cini:(j*k)+(j+1)
+          %calculo do polinomio no ponto (xi + (a-lini)*d, yi + (b-cini)*d)
+          x = xi + (a-lini)*d;
+          y = yi + (b-cini)*d;
+          %p = p(x,y) 
+          p = [1,(x-xi),(x-xi)**2,(x-xi)**3] * coefs * [1,(y-yj),(y-yj)**2,(y-yj)**3];
+          img(a,b,k) = p;
+        endfor
+      endfor
+        
     endfor
   endfor
-  % imwrite (img = matrix PxPx3, filename = arquivo_de_output.img);
+  
+  imwrite (img, "decompressed.png");
+  
 endfunction

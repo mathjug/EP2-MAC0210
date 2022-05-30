34b2f7313af6d674da78874405f2aedd3ac1c4b3
decoNR <94117783+decoNR@users.noreply.github.com>
2022-05-30 00:08:28 -0300

    Add files via upload

---
+++ f_enunciado.m
@@ -0,0 +1,16 @@
+function [f_img] = f_enunciado (p)
+  % função que monta uma imagem a partir de uma função
+  img = zeros(p, p, 3);
+  f_img = "f_img.png";
+  
+  for x = 1:p
+    for y = 1:p
+      img(x,y,1) = sin(x);
+      img(x,y,2) = (sin(y) + sin(x)) / 2;
+      img(x,y,3) = sin(x);
+    endfor
+  endfor
+  
+  imwrite (img, f_img);
+  
+endfunction
